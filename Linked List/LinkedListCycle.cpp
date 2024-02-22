2657e9c874552aa31049ddab797f89f053edfaf0
anoopshetty03 <148759181+anoopshetty03@users.noreply.github.com>
2024-02-23 00:01:18 +0530

    Add files via upload

---
+++ Linked List/LinkedListCycle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // Floyd's cycle detection algorithm
+        if(head == NULL)
+            return false;
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+};
